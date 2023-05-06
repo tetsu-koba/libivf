001153d36e896f6dad7dd298e86930db385da64d
Tetsuyuki Kobayashi <tetu.koba@gmail.com>
2023-05-06 21:00:12 +0900

    Read and Write header at once, assuming the host is little endian.

--- src/ivf.zig
+++ src/ivf.zig
@@ -7,17 +7,17 @@ pub const IVFSignature = "DKIF";
 pub const IVFHeaderSize = 32;
 pub const IVFFrameHeaderSize = 12;
 
-pub const IVFHeader = struct {
-    //signature: [IVFSignature.len]u8, // "DKIF"
-    //version: u16, // File format version (usually 0)
-    //header_size: u16, // Size of the header in bytes (32 bytes for version 0)
+pub const IVFHeader = extern struct {
+    signature: [IVFSignature.len]u8, // "DKIF"
+    version: u16, // File format version (usually 0)
+    header_size: u16, // Size of the header in bytes (32 bytes for version 0)
     fourcc: [4]u8, // Codec used (e.g., "VP80" for VP8, "VP90" for VP9)
     width: u16, // Width of the video in pixels
     height: u16, // Height of the video in pixels
     frame_rate: u32, // Frame rate in Hz
     time_scale: u32, // Time scale (number of time units per second, usually the same as frame rate)
     num_frames: u32, // Total number of frames in the file
-    //unused: u32, // Reserved for future use (set to 0)
+    unused: u32, // Reserved for future use (set to 0)
 };
 
 pub const IVFFrameHeader = struct {
@@ -46,28 +46,16 @@ pub const IVFReader = struct {
 
     fn readIVFHeader(self: *Self) !void {
         var r = self.reader;
-        var sig: [IVFSignature.len]u8 = undefined;
-        if (IVFSignature.len != try r.readAll(&sig) or !mem.eql(u8, &sig, IVFSignature)) {
+        self.header = try r.readStruct(IVFHeader);
+        if (!mem.eql(u8, &self.header.signature, IVFSignature)) {
             return error.IvfFormat;
         }
-        const version = try r.readIntLittle(u16);
-        if (version != 0) {
+        if (self.header.version != 0) {
             return error.IvfFormat;
         }
-        const header_size = try r.readIntLittle(u16);
-        if (header_size != 32) {
+        if (self.header.header_size != 32) {
             return error.IvfFormat;
         }
-        var fourcc = &self.header.fourcc;
-        if (fourcc.len != try r.readAll(fourcc)) {
-            return error.IvfFormat;
-        }
-        self.header.width = try r.readIntLittle(u16);
-        self.header.height = try r.readIntLittle(u16);
-        self.header.frame_rate = try r.readIntLittle(u32);
-        self.header.time_scale = try r.readIntLittle(u32);
-        self.header.num_frames = try r.readIntLittle(u32);
-        _ = try r.readIntLittle(u32); // unused
     }
 
     pub fn readIVFFrameHeader(self: *Self, frame_header: *IVFFrameHeader) !void {
@@ -97,7 +85,17 @@ pub const IVFWriter = struct {
             .writer = file.writer(),
             .frame_count = 0,
         };
-        try self.writeIVFHeader(header);
+        if (!mem.eql(u8, &header.signature, IVFSignature)) {
+            return error.IvfFormat;
+        }
+        if (header.version != 0) {
+            return error.IvfFormat;
+        }
+        if (header.header_size != 32) {
+            return error.IvfFormat;
+        }
+        // Assuming the host is little endian
+        try self.writer.writeAll(@ptrCast([*]const u8, header)[0..@sizeOf(IVFHeader)]);
         return self;
     }
 
@@ -112,19 +110,6 @@ pub const IVFWriter = struct {
         self.file.seekFromEnd(0) catch {
             return;
         };
-    }
-
-    fn writeIVFHeader(self: *Self, header: *const IVFHeader) !void {
-        try self.writer.writeAll(IVFSignature);
-        try self.writer.writeIntLittle(u16, 0);
-        try self.writer.writeIntLittle(u16, IVFHeaderSize);
-        try self.writer.writeAll(&header.fourcc);
-        try self.writer.writeIntLittle(u16, header.width);
-        try self.writer.writeIntLittle(u16, header.height);
-        try self.writer.writeIntLittle(u32, header.frame_rate);
-        try self.writer.writeIntLittle(u32, header.time_scale);
-        try self.writer.writeIntLittle(u32, header.num_frames);
-        try self.writer.writeIntLittle(u32, 0);
     }
 
     pub fn writeIVFFrame(self: *Self, frame: []const u8, timestamp: u64) !void {
