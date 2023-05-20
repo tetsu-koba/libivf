156ae885ede9f2d2e217b8671780887e8057f987
Tetsuyuki Kobayashi <tetu.koba@gmail.com>
2023-05-20 22:32:32 +0900

    Remove mem.sliceTo for args

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const fs = std.fs;
 const io = std.io;
-const mem = std.mem;
 const os = std.os;
 
 const IVF = @import("ivf.zig");
@@ -15,7 +14,7 @@ pub fn main() !void {
         std.debug.print("Usage: {s} ivf-file\n", .{args[0]});
         os.exit(1);
     }
-    const filename = std.mem.sliceTo(args[1], 0);
+    const filename = args[1];
     try dumpIVFFile(filename);
 }
 
